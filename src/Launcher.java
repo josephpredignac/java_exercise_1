bec3e4f86c1ef6355ec3d3abd906ff3aafab307a
joseph.predignac <joseph.predignac@epita.fr>
2022-02-11 13:05:48 +0100

    v4

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
 
 public class Launcher {
+    public static int fibo(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        return (fibo(n - 1) + fibo(n - 2));
+    }
+
     public static void main(String[] args) {
         System.out.println("Salut bg du 92");
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
         while (!command.equals("quit")) {
-            System.out.println("Unknown command");
+            if (command.equals("fibo")) {
+                System.out.println("Donne un n");
+                int n = sc.nextInt();
+                command = sc.nextLine();
+                System.out.println(fibo(n));
+            }
+            else {
+                System.out.println("Unknown command");
+            }
             command = sc.nextLine();
         }
     }
